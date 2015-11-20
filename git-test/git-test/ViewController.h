34559cf619a1d31aef8109a497e9669eded221ee
梁炜宏 <Hiro@liangweihongdemac-mini.local>
2015-11-20 16:53:43 +0800

    git测试

---
+++ git-test/git-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git-test
+//
+//  Created by 梁炜宏 on 15/11/20.
+//  Copyright © 2015年 梁炜宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git-test/git-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git-test
+//
+//  Created by 梁炜宏 on 15/11/20.
+//  Copyright © 2015年 梁炜宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
